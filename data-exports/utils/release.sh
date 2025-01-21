69f7a6c4f7fc59f7733a40788328d02ae6101152
Iakov Gan <iakov@amazon.com>
2025-01-21 10:41:49 +0100

    change versions for data exports script

--- data-exports/utils/release.sh
+++ data-exports/utils/release.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # shellcheck disable=SC2016,SC2086,SC2162
-# This script can be used for release
+# This script can be used for release.
 
-export CENTRAL_BUCKET=aws-managed-cost-intelligence-dashboards
-
-code_path=$(git rev-parse --show-toplevel)/data-exports/deploy
-version=$(jq -r .version data-collection/utils/version.json)
-
-echo "sync files"
-aws s3 sync $code_path/       s3://$CENTRAL_BUCKET/cfn/data-exports/$version/ --delete
-aws s3 sync $code_path/       s3://$CENTRAL_BUCKET/cfn/data-exports/latest/ --delete
-aws s3 sync $code_path/       s3://$CENTRAL_BUCKET/cfn/ #legacy location
+CENTRAL_BUCKET=aws-managed-cost-intelligence-dashboards
+files=("data-exports-aggregation" "cur-aggregation")
+for file in "${files[@]}"; do
+    #Here data export stack and legacy cur aggregation have their own versions
+    version=$(grep '^Description:' "data-exports/deploy/${file}.yaml" | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+')
+    source_path="data-exports/deploy/${file}.yaml"
+    echo $source_path
+    aws s3 sync "$source_path" "s3://$CENTRAL_BUCKET/cfn/data-exports/$version/${file}.yaml"
+    aws s3 sync "$source_path" "s3://$CENTRAL_BUCKET/cfn/data-exports/latest/${file}.yaml"
+    aws s3 sync "$source_path" "s3://$CENTRAL_BUCKET/cfn/${file}.yaml"
+done
