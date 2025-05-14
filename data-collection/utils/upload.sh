cbe913505256aa2fdcaef87611560816f790931d
Iakov Gan <iakov@amazon.com>
2025-05-14 21:19:55 +0200

    fix version path for tests

--- data-collection/utils/upload.sh
+++ data-collection/utils/upload.sh
@@ -13,6 +13,6 @@ code_path=$(git rev-parse --show-toplevel)/data-collection/deploy
 version=$(jq -r '.version' data-collection/utils/version.json)
 
 echo "Sync to $bucket"
-aws s3 sync $code_path/       s3://$bucket/cfn/data-collection/
-aws s3 sync $code_path/       s3://$bucket/cfn/data-collection/$version/ --delete
+#aws s3 sync $code_path/       s3://$bucket/cfn/data-collection/ --delete
+aws s3 sync $code_path/       s3://$bucket/cfn/data-collection/v$version/ --delete
 echo 'Done'
